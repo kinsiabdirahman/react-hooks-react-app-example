455f22391f93f3c49fca2aada28a61b7deb05dd7
kinsiabdirahman <kinsiabdirahman@outlook.com>
2024-02-04 00:27:10 +0300

    All deliverables met

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
+//The "entry point" into our application — 
+//the first JavaScript code that will run when our app starts up
 ReactDOM.render(<App />, document.getElementById("root"));
